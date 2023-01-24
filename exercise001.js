0f3a631d15588e124fc60425c8bdfdbd25fe9bd0
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-24 16:45:00 +0000

    calls promise and uses then and catch

--- exercise001.js
+++ exercise001.js
@@ -12,3 +12,6 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // Your solution(s) to exercise001 here!
+promise
+	.then(result => console.log(`Yay! Promise resolved with response: ${result}`))
+	.catch(error => console.log(`Boo. Promise rejected with response: ${error}`));
